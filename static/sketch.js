8d83de16eff36efacd4b68355f7df051d2ebc7b9
Jason Zhang <zhangjt@umich.edu>
2024-05-15 10:55:23 -0400

    new website structure

---
+++ static/sketch.js
@@ -0,0 +1,16 @@
+
+function preload() {
+
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0, 100);
+ }
+
+ function draw() {
+    textFont('Verdana', 50)
+    textAlign(CENTER, CENTER);
+    text('Jason Zhang', width/2, height/2)
+    
+ }
