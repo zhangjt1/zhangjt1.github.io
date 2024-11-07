8fa59b3cfec0445f767d0ad60293aac0133e6425
Jason Zhang <zhangjt@umich.edu>
2024-11-06 23:07:45 -0500

    added camera tracked clap ripples

--- ocean.js
+++ ocean.js
@@ -5,6 +5,8 @@ Since it calculate a lot in the setup().
 If you want to short the processing time, try change the frmLen value smaller.
 like 90 or 60.
 */
+
+// Ocean
 const density = '011===--.  ';
 const len = density.length;
 
@@ -18,6 +20,45 @@ const randScalesY = [];
 let maxNoise = 0;
 const res = 15;
 
+// Ripples
+let video;
+
+let bodyPose;
+
+let poses = [];
+
+let ripplers = [];
+
+let handsClose = false;
+
+class Rippler {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.d = 0;
+    this.alpha = 255;
+  }
+  draw() {
+    this.d += 1;
+    this.alpha -= 1;
+
+    // Draw three circles with decreasing opacity as they get larger
+    for (let i = 0; i < 3; i++) {
+      let diameter = this.d + i * 20; // Each circle grows by 10 pixels in diameter
+      stroke(255, 255, 255, this.alpha - (i * 50)); // Set the stroke color with calculated opacity
+      circle(this.x, this.y, diameter);
+    }
+  }
+}
+
+function preload() {
+  bodyPose = ml5.bodyPose("MoveNet", {flipped: true});
+}
+
+function gotPoses(results) {
+  poses = results;
+}
+
 function setup(){
   createCanvas(windowWidth, windowHeight+200);
   angleMode(DEGREES);
@@ -25,6 +66,12 @@ function setup(){
   strokeWeight(12);
   frameRate(15);
   noCursor();
+
+  video = createCapture(VIDEO, {flipped: true});
+  video.size(windowWidth, windowHeight+200);
+  video.hide();
+
+  bodyPose.detectStart(video, gotPoses);
 
   // randomSeed(70);
   for(let i = 0; i < 160; i++){
@@ -94,10 +141,47 @@ function draw(){
     }
   }
 
-  // for(let i = 0; i < points[frameIndex].length; i++){
-  //   circle(points[frameIndex][i].x, points[frameIndex][i].y, 5);
-  // }
-  
+  // Ripples
+  if (poses.length > 0) {
+    for (let pose of poses) {
+      if (pose.confidence > 0.1) {
+        let rx = pose.right_wrist.x;
+        let ry = pose.right_wrist.y;
+        let lx = pose.left_wrist.x;
+        let ly = pose.left_wrist.y;
+
+        // fill(255, 0, 0);
+        // circle(rx, ry, 20);
+        // fill(0, 255, 0);
+        // circle(lx, ly, 20);
+
+        let d = dist(rx, ry, lx, ly);
+
+        if (d < 100) {
+          let x = (rx + lx) / 2;
+          let y = (ry + ly) / 2;
+          if (!handsClose) {
+            ripplers.push(new Rippler(x, y));
+            handsClose = true;
+          }
+        }
+        else {
+          handsClose = false;
+        }
+      }
+    }
+  }
+
+  strokeWeight(3);
+  noFill();
+
+  for (let i = ripplers.length - 1; i >= 0; i--) {
+    ripplers[i].draw();
+    // Remove the Rippler if its diameter exceeds 50
+    if (ripplers[i].d > 255) {
+      ripplers.splice(i, 1);
+    }
+  }
 }
 
 function mousePressed() {
